e877e17dbccdd3032df5405398e69406c471c872
Evan Armstrong <earmstrong@lcs.on.ca>
2021-02-19 12:31:26 -0500

    Started asynchronous app

--- asynchronousApp/ContentView.swift
+++ asynchronousApp/ContentView.swift
@@ -8,11 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+   @State var someText = "Hello, World"
+    
     var body: some View {
-        Text("Hello, world!")
+        
+        VStack {
+            
+        Text(someText)
             .padding()
+            .onAppear(){
+    
+    fetchQuote ()
+            }
+            }
     }
 }
+
+    func fetchQuote() {
+        
+        let url = URL (string: "https://forismatic.com/en/")!
+        
+        var request = URLRequest(url: url)
+        request.setValue("application/json",
+                         forHTTPHeaderField: "Accept")
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            guard let Quote = data else {
+                
+                print("No data in response: \(error?.localizedDescription ?? "Unknown error")")
+
+                // Don't continue past this point
+                return
+
+            }
+            if let DecodedAsynchroQuote = try? JSONDecoder().decode(Asynchronous.self, from: quote) {
+
+                // DEBUG:
+                print("Quote data decoded from JSON successfully")
+                
+                print("""
+                    The Quote is:
+                    \(DecodedAsynchronousQuote.quote)
+                    """)
+
+                // Now, update the UI on the main thread
+                DispatchQueue.main.async {
+                    someText = DecodedAsynchronousQuote.quote
+                }
+
+            } else {
+
+                print("Could not decode JSON into an instance of the DadJoke structure.")
+
+            }
+
+        }.resume()
+        // NOTE: Invoking the resume() function
+        // on the dataTask closure is key. The request will not
+        // run, otherwise.
+
+    }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ asynchronousApp/Preview Content/Asynchronous.swift
@@ -0,0 +1,14 @@
+//
+//  Asynchronous.swift
+//  asynchronousApp
+//
+//  Created by Evan Armstrong on 2021-02-19.
+//
+
+import Foundation
+
+struct AsynchronousQuote: Decodable {
+    let id: String
+    let quote: String
+    let status: Int
+}
