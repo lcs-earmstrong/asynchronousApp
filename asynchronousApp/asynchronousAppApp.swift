c2693e3d1d87ff3269974f285d727fc1fd046d2c
Evan Armstrong <earmstrong@lcs.on.ca>
2021-02-19 11:50:56 -0500

    Initial Commit

---
+++ asynchronousApp/asynchronousAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  asynchronousAppApp.swift
+//  asynchronousApp
+//
+//  Created by Evan Armstrong on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct asynchronousAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
